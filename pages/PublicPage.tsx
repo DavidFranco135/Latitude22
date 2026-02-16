eb0eff1ce7a276ddb3bd6ebec86888a684cfb07f
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-16 00:13:26 -0300

    Update PublicPage.tsx

--- pages/PublicPage.tsx
+++ pages/PublicPage.tsx
@@ -262,9 +262,10 @@ const PublicPage: React.FC = () => {
               Exclusividade & Requinte
             </span>
           </div>
-          <h2 className="mb-8 font-serif text-5xl md:text-8xl text-yellow leading-tight drop-shadow-2xl">
-            Latitude22
-          </h2>
+          <h2 className="mb-8 font-serif text-5xl md:text-8xl text-yellow-400 leading-tight drop-shadow-2xl">
+  Latitude22
+</h2>
+
           <div className="mt-12 flex flex-col sm:flex-row items-center justify-center gap-6">
             <button 
               onClick={() => scrollToSection('galeria')}
